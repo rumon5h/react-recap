4a1643b5c6ffd16437fd4b12c3ac2d3a6b2d60d2
rumon5h <mdrumonkhan48519@gmail.com>
2024-05-26 09:52:30 +0600

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,41 +20,32 @@ const Navbar = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/aboutus">About Us</Link></li>
+            <li><Link to="/contactus">Contact Us</Link></li>
+            
+            <li><Link to="/dashboard">Dashboard</Link></li>
             </ul>
           </div>
           <Link to="/" className="btn btn-ghost text-xl">daisyUI</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><Link to="/">Home</Link></li>
+            
+          <li><Link to="/">Home</Link></li>
+            <li><Link to="/services">Services</Link></li>
             <li><Link to="/products">Products</Link></li>
             <li><Link to="/aboutus">About Us</Link></li>
             <li><Link to="/contactus">Contact Us</Link></li>
             
             <li><Link to="/dashboard">Dashboard</Link></li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </details>
-            </li>
-            <li><Link to="/services">Services</Link></li>
           </ul>
         </div>
         <div className="navbar-end">
-          {user? <li><button onClick={handleLogOut} className='btn btn-ghost'>Log Out</button></li> : <Link to="/login" className="btn">LOGIN</Link>}
+          {user? <li className='list-none'><button onClick={handleLogOut} className='btn btn-ghost'>Log Out</button></li> : 
+          <Link to="/login" className="btn">LOGIN</Link>}
         </div>
       </div>
     </>
