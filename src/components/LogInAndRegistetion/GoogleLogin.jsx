556bcad3c62ab9474b42b1baea4552d46593ef72
rumon5h <mdrumonkhan48519@gmail.com>
2024-06-01 10:56:06 +0600

    updated

--- src/components/LogInAndRegistetion/GoogleLogin.jsx
+++ src/components/LogInAndRegistetion/GoogleLogin.jsx
@@ -1,20 +1,43 @@
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc';
 import useAuth from '../../hooks/useAuth';
+import toast from 'react-hot-toast';
 
 const GoogleLogin = () => {
-    const {googleLogin} = useAuth();
+  const { googleLogin } = useAuth();
 
-    const handleGoogleLogin = () => {
-        googleLogin();
-    }
+  const handleGoogleLogin = () => {
+    googleLogin().then((data) => {
+      if (data?.user?.email) {
+        const userInfo = {
+          email: data?.user?.email,
+          name: data?.user?.displayName
+        };
+
+        fetch("http://localhost:5000/users", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(userInfo),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.acknowledged) {
+              toast.success("Loged in successfully");
+            }
+          });
+      }
+    });
+  }
 
   return (
     <div onClick={handleGoogleLogin} className='btn w-full'>
-        <div className='flex items-center gap-2'>
-            <FcGoogle size={24}/>
-            <p>Google</p>
-        </div>
+      <div className='flex items-center gap-2'>
+        <FcGoogle size={24} />
+        <p>Google</p>
+      </div>
     </div>
   )
 }
